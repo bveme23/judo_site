2e0a61690268dac9c233c13cf23d4125db6e0fd9
bveme23 <bveme23@gmail.com>
2025-12-11 10:41:28 -0800

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -51,7 +51,7 @@ export default function RootLayout({
       <body
         className={`${notoSans.variable} ${notoSerif.variable} ${inter.variable} antialiased`}
         style={{
-          backgroundImage: "url('/judo_jap.png')",
+          backgroundImage: "url('/herojudo.png')",
           backgroundSize: 'cover',
           backgroundPosition: 'center',
           backgroundRepeat: 'no-repeat',
